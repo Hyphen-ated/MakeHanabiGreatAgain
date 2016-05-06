b4c3428cc814fb2390c740963adc8f3e500a46bd
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2016-05-05 22:57:15 -0700

    add build infra to make userscript

---
+++ package.py
@@ -0,0 +1,14 @@
+version = "0.1"
+
+with open('template.txt', 'r') as template_file:
+    template = template_file.read()
+    with open('ui.js', 'r') as ui_file:
+        ui = ui_file.read()
+        with open('lobby.js') as lobby_file:
+            lobby = lobby_file.read()
+            output = template.replace('%LOBBY.JS%', lobby)\
+                            .replace('%UI.JS%', ui)\
+                            .replace('%VERSION%', version)
+            output_file = open("make_hanabi_great_again-" + version + ".user.js", "w")
+            output_file.write(output)
+            output_file.close()
